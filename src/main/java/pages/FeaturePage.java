a4c6f3ca86b90db7892ecbbc4f678ae1e1c49a44
TanyaSaha <ravindra.kamisetty@gmail.com>
2025-03-03 14:42:26 +0530

    added feature page 2

--- src/main/java/pages/FeaturePage.java
+++ src/main/java/pages/FeaturePage.java
@@ -4,7 +4,9 @@ public class FeaturePage {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Feature Page");
+		System.out.println("Feature Page 1");
+		
+		System.out.println("Feature Page 2");
 
 	}
 
